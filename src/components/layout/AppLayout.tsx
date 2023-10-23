aa4edcb5d7da623084ce691791842908a60e78f9
Boki87 <peric.bojan87@gmail.com>
2023-10-23 10:09:32 +0200

    DANGER: set all routes to public for testing

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -50,6 +50,7 @@ export const AppLayout = ({ isPrivate = false }: { isPrivate?: boolean }) => {
   }
 
   let showRoutes = isPrivate ? !!user : true;
+  showRoutes = true;
   return (
     <>
       {showRoutes ? (
